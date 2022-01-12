8f740068916440edf95f6753e92d5b8273ddb93b
yangguojun <544759698@qq.com>
2022-01-12 19:16:02 +0800

    day20220112

---
+++ src/main/java/com/yang/practice/geek/qinchao/lesson45/ClimbStair.java
@@ -0,0 +1,24 @@
+package com.yang.practice.geek.qinchao.lesson45;
+
+/**
+ * 爬楼梯，一次只能爬一层或两层，求爬到N层有多少种爬法
+ *
+ * @Author: yangguojun01
+ * @Date: 2022/1/12
+ */
+public class ClimbStair {
+
+    public int climbStairs(int n) {
+        if (n == 0 || n == 1 || n == 2) {
+            return n;
+        }
+        int[] memo = new int[n];
+        memo[0] = 1;
+        memo[1] = 2;
+        for (int i = 2; i < n; i++) {
+            memo[i] = memo[i - 1] + memo[i - 2];
+        }
+        return memo[n - 1];
+    }
+
+}
