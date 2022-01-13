618b7f14cf2b27d4c7b2732f793bb1f50318ba35
yangguojun <544759698@qq.com>
2022-01-14 02:31:27 +0800

    day20220114

--- src/main/java/com/yang/practice/geek/qinchao/lesson48/CommonStock.java
+++ src/main/java/com/yang/practice/geek/qinchao/lesson48/CommonStock.java
@@ -9,9 +9,9 @@ package com.yang.practice.geek.qinchao.lesson48;
 public class CommonStock {
 
     /***
-     * 买卖一次算交易一次
+     * 最大收益
      * @param prices
-     * @param k 交易次数
+     * @param k 一对买卖的次数
      * @return
      */
     public int maxProfit(int k, int[] prices) {
@@ -19,8 +19,10 @@ public class CommonStock {
         if (n <= 1) {
             return 0;
         }
-        // 买卖一次算交易一次，大于n/2相当于没有交易次数限制
-        k = Math.min(k, n / 2);
+        // k转化为买一次和卖一次都算作一次交易，更符合常规认知！！！
+        k = 2 * k;
+        // k大于n相当于没有交易次数限制
+        k = Math.min(k, n);
         /*dp定义：dp[i][j][k]代表 第i天交易了k次时的最大利润，其中j代表当天是否持有股票，0不持有，1持有*/
         int[][][] maxProfit = new int[n][2][k + 1];
         for (int kTmp = 0; kTmp <= k; kTmp++) {
@@ -33,19 +35,10 @@ public class CommonStock {
         dp[i][0][k]，当天不持有股票时，看前一天的股票持有情况
         dp[i][1][k]，当天持有股票时，看前一天的股票持有情况*/
         for (int i = 1; i < n; i++) {
-            for (int kTmp = 0; kTmp <= k; kTmp++) {
-                // kTmp==0时并不是取两数的最大值，而是等于0
-                // TODO:为什么kTmp不能从1开始？？？
-                maxProfit[i][0][kTmp] =
-                        kTmp == 0 ? 0 : Math.max(maxProfit[i - 1][0][kTmp], maxProfit[i - 1][1][kTmp - 1] + prices[i]);
-                maxProfit[i][1][kTmp] = Math.max(maxProfit[i - 1][0][kTmp] - prices[i], maxProfit[i - 1][1][kTmp]);
+            for (int kTmp = 1; kTmp <= k; kTmp++) {
+                maxProfit[i][0][kTmp] = Math.max(maxProfit[i - 1][0][kTmp], maxProfit[i - 1][1][kTmp - 1] + prices[i]);
+                maxProfit[i][1][kTmp] = Math.max(maxProfit[i - 1][0][kTmp - 1] - prices[i], maxProfit[i - 1][1][kTmp]);
             }
-            //            for (int kTmp = 1; kTmp <= k; kTmp++) {
-            //                maxProfit[i][0][kTmp] = Math.max(maxProfit[i - 1][0][kTmp], maxProfit[i - 1][1][kTmp -
-            //                1] + prices[i]);
-            //                maxProfit[i][1][kTmp] = Math.max(maxProfit[i - 1][0][kTmp] - prices[i], maxProfit[i -
-            //                1][1][kTmp]);
-            //            }
         }
         return maxProfit[n - 1][0][k];
     }
