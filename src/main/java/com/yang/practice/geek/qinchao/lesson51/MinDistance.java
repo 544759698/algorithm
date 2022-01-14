3b17fc88d45258c89ff6b23a2f03fdccdea11004
yangguojun <544759698@qq.com>
2022-01-15 01:16:13 +0800

    day20220115

---
+++ src/main/java/com/yang/practice/geek/qinchao/lesson51/MinDistance.java
@@ -0,0 +1,46 @@
+package com.yang.practice.geek.qinchao.lesson51;
+
+/**
+ * leetcode 72 两个单词的最小距离，可以删除、插入、替换
+ *
+ * @Author: yangguojun01
+ * @Date: 2022/1/15
+ */
+public class MinDistance {
+
+    public int minDistance(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+
+        // 有一个字符串为空串
+        if (m * n == 0) {
+            return m + n;
+        }
+
+        // DP 数组
+        int[][] dp = new int[m + 1][n + 1];
+
+        // 边界状态初始化
+        for (int i = 0; i < m + 1; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 0; j < n + 1; j++) {
+            dp[0][j] = j;
+        }
+
+        // 计算所有 DP 值
+        for (int i = 1; i < m + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                int left = dp[i - 1][j] + 1;
+                int down = dp[i][j - 1] + 1;
+                int left_down = dp[i - 1][j - 1];
+                if (word1.charAt(i - 1) != word2.charAt(j - 1)) {
+                    left_down += 1;
+                }
+                dp[i][j] = Math.min(left, Math.min(down, left_down));
+            }
+        }
+        return dp[m][n];
+    }
+
+}
