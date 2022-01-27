719c670942e76509a84cabc9604701fc4ed9987a
yangguojun <544759698@qq.com>
2022-01-27 23:07:13 +0800

    day20220127

--- src/main/java/com/yang/practice/geek/qinchao/lesson35/Sqrt.java
+++ src/main/java/com/yang/practice/geek/qinchao/lesson35/Sqrt.java
@@ -1,35 +1,36 @@
 package com.yang.practice.geek.qinchao.lesson35;
 
+import com.yang.practice.geek.qinchao.lesson06.MidNode;
+
 /**
- * 求平方根
+ * leetcode69 求平方根
  *
  * @Author: yangguojun01
  * @Date: 2022/1/7
  */
 public class Sqrt {
 
-    public double getSqrt(int n) {
-        if (n < 1) {
-            return -1;
-        }
-        if (n == 1) {
-            return 1;
-        }
+    public int mySqrt(int x) {
+        double ans = 0;
         double left = 0;
-        double right = n;
-        while (right - left > 1e-5) {
-            double mid = left + (right - left) / 2.0;
-            if (mid > (double) n / mid) {
+        double right = x;
+        while ((right - left) > 0.00000001) {
+            double mid = left + (right - left) / 2;
+            ans = mid;
+            // 可以防止越界
+            if (mid > x / mid) {
                 right = mid;
             } else {
                 left = mid;
             }
         }
-        return left;
+        System.out.println(ans);
+        return (int) right;
+        //        return (int) (left + right) / 2;
     }
 
     public static void main(String[] args) {
         Sqrt s = new Sqrt();
-        System.out.println(s.getSqrt(1));
+        System.out.println(s.mySqrt(36));
     }
 }
