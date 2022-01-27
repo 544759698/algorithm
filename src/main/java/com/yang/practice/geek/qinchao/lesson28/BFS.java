417c8d7a30747bfa517b3ad66122d8e1020514a9
yangguojun <544759698@qq.com>
2022-01-27 10:26:50 +0800

    day20220127

--- src/main/java/com/yang/practice/geek/qinchao/lesson28/BFS.java
+++ src/main/java/com/yang/practice/geek/qinchao/lesson28/BFS.java
@@ -6,20 +6,48 @@ import java.util.List;
 import java.util.Queue;
 
 /**
+ * leetcode102 二叉树的层序遍历
+ *
  * @Author: yangguojun01
  * @Date: 2022/1/4
  */
 public class BFS {
 
-    public List<Node> getBFS(Node node) {
-        List<Node> ret = new ArrayList<>();
-        if (node == null) {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        List<List<Integer>> ret = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            // TODO queue的size就是每层的节点数
+            int levelSize = queue.size();
+            List<Integer> levelRet = new ArrayList<>();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode curr = queue.poll();
+                levelRet.add(curr.val);
+                if (curr.left != null) {
+                    queue.add(curr.left);
+                }
+                if (curr.right != null) {
+                    queue.add(curr.right);
+                }
+            }
+            ret.add(levelRet);
+        }
+        return ret;
+    }
+
+    public List<TreeNode> getBFS(TreeNode treeNode) {
+        List<TreeNode> ret = new ArrayList<>();
+        if (treeNode == null) {
             return ret;
         }
-        Queue<Node> queue = new LinkedList<>();
-        queue.add(node);
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(treeNode);
         while (!queue.isEmpty()) {
-            Node first = queue.poll();
+            TreeNode first = queue.poll();
             ret.add(first);
             if (first.left != null) {
                 queue.add(first.left);
@@ -32,32 +60,32 @@ public class BFS {
     }
 
     public static void main(String[] args) {
-        Node n1 = new Node(1);
-        Node n2 = new Node(2);
-        Node n3 = new Node(3);
-        Node n4 = new Node(4);
-        Node n5 = new Node(5);
-        Node n6 = new Node(6);
+        TreeNode n1 = new TreeNode(1);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(3);
+        TreeNode n4 = new TreeNode(4);
+        TreeNode n5 = new TreeNode(5);
+        TreeNode n6 = new TreeNode(6);
         n1.left = n2;
         n1.right = n3;
         n2.left = n4;
         n2.right = n5;
         n3.left = n6;
         BFS b = new BFS();
-        List<Node> ret = b.getBFS(n1);
-        for (Node node : ret) {
-            System.out.println(node.value);
+        List<TreeNode> ret = b.getBFS(n1);
+        for (TreeNode treeNode : ret) {
+            System.out.println(treeNode.val);
         }
     }
 
 }
 
-class Node {
-    int value;
-    Node left;
-    Node right;
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
 
-    public Node(int value) {
-        this.value = value;
+    public TreeNode(int val) {
+        this.val = val;
     }
 }
