418329e27f7059d1a2058959c26b4df7db9752b3
yangguojun <544759698@qq.com>
2022-01-28 17:46:41 +0800

    day20220128

--- src/main/java/com/yang/practice/geek/qinchao/lesson25/SellStockN.java
+++ src/main/java/com/yang/practice/geek/qinchao/lesson25/SellStockN.java
@@ -2,12 +2,14 @@ package com.yang.practice.geek.qinchao.lesson25;
 
 /**
  * leetcode122 买卖股票，最简单的题，给一组价格，不限制买卖次数
+ * leetcode121 买卖股票，给一组价格，买卖一次
  *
  * @Author: yangguojun01
  * @Date: 2022/1/4
  */
 public class SellStockN {
 
+    // 122
     public int getMaxProfit(int[] prices) {
         if (prices == null || prices.length == 0) {
             return 0;
@@ -21,10 +23,28 @@ public class SellStockN {
         return maxProfit;
     }
 
+    // 121
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else if (prices[i] - minPrice > maxProfit) {
+                maxProfit = prices[i] - minPrice;
+            }
+        }
+        return maxProfit;
+    }
+
     public static void main(String[] args) {
         SellStockN s = new SellStockN();
         int[] prices = {7, 1, 5, 3, 6, 4};
         System.out.println(s.getMaxProfit(prices));
+        System.out.println(s.maxProfit(prices));
     }
 
 }
