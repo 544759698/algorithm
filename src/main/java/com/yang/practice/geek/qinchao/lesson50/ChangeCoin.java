e930e0c55cb3eeefb9fe28d7048a58cad0ff40f9
yangguojun <544759698@qq.com>
2022-01-29 00:31:04 +0800

    20220129

--- src/main/java/com/yang/practice/geek/qinchao/lesson50/ChangeCoin.java
+++ src/main/java/com/yang/practice/geek/qinchao/lesson50/ChangeCoin.java
@@ -3,7 +3,7 @@ package com.yang.practice.geek.qinchao.lesson50;
 import java.util.Arrays;
 
 /**
- * leetcode 322 使用最小硬币数换目标钱数
+ * leetcode322 使用最小硬币数换目标钱数
  *
  * @Author: yangguojun01
  * @Date: 2022/1/14
@@ -16,10 +16,12 @@ public class ChangeCoin {
         }
         int[] memo = new int[amount + 1];
         Arrays.fill(memo, -1);
+        // 这里必须赋值
         memo[0] = 0;
         for (int i = 1; i <= amount; i++) {
             int result = -1;
             for (int j = 0; j < coins.length; j++) {
+                // coins[j]可以用且i-coins[j]有解，最终才能有解，然后判断是不是最小硬币数！！！
                 if (i - coins[j] >= 0) {
                     int tmp = memo[i - coins[j]];
                     if (tmp != -1 && (result == -1 || (tmp + 1) < result)) {
@@ -33,9 +35,9 @@ public class ChangeCoin {
     }
 
     public static void main(String[] args) {
-        int[] coins = {3, 4};
+        int[] coins = {2};
         ChangeCoin c = new ChangeCoin();
-        System.out.println(c.changeCoin(coins, 8));
+        System.out.println(c.changeCoin(coins, 5));
     }
 
 }
