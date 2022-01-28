31dce4cc9f4ac09b50178deb1230f7b0582b21f1
yangguojun <544759698@qq.com>
2022-01-28 19:50:25 +0800

    20220128

--- src/main/java/com/yang/practice/geek/qinchao/lesson48/CommonStock.java
+++ src/main/java/com/yang/practice/geek/qinchao/lesson48/CommonStock.java
@@ -1,7 +1,7 @@
 package com.yang.practice.geek.qinchao.lesson48;
 
 /**
- * leetcode 121 122 123 309 188 714 买卖股票系列
+ * leetcode (121 122 lesson25) 188 123 309 714 买卖股票系列
  *
  * @Author: yangguojun01
  * @Date: 2022/1/13
@@ -23,16 +23,16 @@ public class CommonStock {
         k = 2 * k;
         // k大于n相当于没有交易次数限制
         k = Math.min(k, n);
-        /*dp定义：dp[i][j][k]代表 第i天交易了k次时的最大利润，其中j代表当天是否持有股票，0不持有，1持有*/
+        // dp定义：dp[i][j][k]代表 第i天交易了k次时的最大利润，其中j代表当天是否持有股票，0不持有，1持有
         int[][][] maxProfit = new int[n][2][k + 1];
         for (int kTmp = 0; kTmp <= k; kTmp++) {
             // 赋初始值，第一天没有股票就为0，有股票就为-prices[0]
             maxProfit[0][0][kTmp] = 0;
             maxProfit[0][1][kTmp] = -prices[0];
         }
-        /*状态方程：
-        dp[i][0][k]，当天不持有股票时，看前一天的股票持有情况
-        dp[i][1][k]，当天持有股票时，看前一天的股票持有情况*/
+        // 状态方程：
+        // dp[i][0][k]，当天不持有股票时，看前一天的股票持有情况
+        // dp[i][1][k]，当天持有股票时，看前一天的股票持有情况
         for (int i = 1; i < n; i++) {
             for (int kTmp = 1; kTmp <= k; kTmp++) {
                 maxProfit[i][0][kTmp] = Math.max(maxProfit[i - 1][0][kTmp], maxProfit[i - 1][1][kTmp - 1] + prices[i]);
@@ -43,7 +43,7 @@ public class CommonStock {
     }
 
     public static void main(String[] args) {
-        int[] prices = {3, 2, 6, 5, 0, 3};
+        int[] prices = {2, 4, 1};
         CommonStock s = new CommonStock();
         System.out.println(s.maxProfit(2, prices));
     }
