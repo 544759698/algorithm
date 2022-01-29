5ab5c8845758f26bae9f1c52ad0687b9d9bc7053
yangguojun <544759698@qq.com>
2022-01-29 11:48:52 +0800

    day20220129

--- src/main/java/com/yang/practice/geek/qinchao/lesson51/MinDistance.java
+++ src/main/java/com/yang/practice/geek/qinchao/lesson51/MinDistance.java
@@ -2,6 +2,7 @@ package com.yang.practice.geek.qinchao.lesson51;
 
 /**
  * leetcode 72 两个单词的最小距离，可以删除、插入、替换
+ * TODO 待复习，记住！！！
  *
  * @Author: yangguojun01
  * @Date: 2022/1/15
@@ -17,10 +18,10 @@ public class MinDistance {
             return m + n;
         }
 
-        // DP 数组
+        // DP数组 word1的前i个字符和word2的前j个字符的编辑距离
         int[][] dp = new int[m + 1][n + 1];
 
-        // 边界状态初始化
+        // 边界状态初始化 一个子串到空字符串的编辑距离等于子串的长度
         for (int i = 0; i < m + 1; i++) {
             dp[i][0] = i;
         }
@@ -28,7 +29,9 @@ public class MinDistance {
             dp[0][j] = j;
         }
 
-        // 计算所有 DP 值
+        // 计算所有DP值 本质不同的操作实际上只有三种：
+        // 1.在word1子串中插入一个字符；2.在word2子串中插入一个字符；3.修改word1或word2子串的一个字符
+        // 三者取最小
         for (int i = 1; i < m + 1; i++) {
             for (int j = 1; j < n + 1; j++) {
                 int left = dp[i - 1][j] + 1;
