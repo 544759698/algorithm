5cb2d81cdf43f48b966dbe736c99568071004389
yangguojun <544759698@qq.com>
2022-02-01 16:28:36 +0800

    day20220201

--- src/main/java/com/yang/practice/geek/qinchao/lesson30/GenerateParenthesis.java
+++ src/main/java/com/yang/practice/geek/qinchao/lesson30/GenerateParenthesis.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * leetcode22 数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
- * TODO 待复习
+ * leetcode22 数字n代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且有效的括号组合。
+ * TODO 待复习，回溯法
  *
  * @Author: yangguojun01
  * @Date: 2022/1/6
