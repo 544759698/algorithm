a07e79c3824c8c6aa5af2905433b8ece658f7c3d
yangguojun <544759698@qq.com>
2022-02-03 21:35:53 +0800

    day20220203

--- src/main/java/com/yang/practice/geek/qinchao/lesson19/Ancestor.java
+++ src/main/java/com/yang/practice/geek/qinchao/lesson19/Ancestor.java
@@ -9,49 +9,67 @@ package com.yang.practice.geek.qinchao.lesson19;
  */
 public class Ancestor {
 
-    public int findAncestor(Node node, int value1, int value2) {
-        if (node == null) {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == p.val || root.val == q.val) {
+            return root;
+        }
+        TreeNode leftNode = lowestCommonAncestor(root.left, p, q);
+        TreeNode rightNode = lowestCommonAncestor(root.right, p, q);
+        if (leftNode == null && rightNode == null) {
+            return null;
+        }
+        if (leftNode != null && rightNode != null) {
+            return root;
+        }
+        return leftNode != null ? leftNode : rightNode;
+    }
+
+    public int findAncestor(TreeNode treeNode, int value1, int value2) {
+        if (treeNode == null) {
             return -1;
         }
-        if (node.value == value1 || node.value == value2) {
-            return node.value;
+        if (treeNode.val == value1 || treeNode.val == value2) {
+            return treeNode.val;
         }
-        int leftValue = findAncestor(node.left, value1, value2);
-        int rightValue = findAncestor(node.right, value1, value2);
+        int leftValue = findAncestor(treeNode.left, value1, value2);
+        int rightValue = findAncestor(treeNode.right, value1, value2);
         // find 2
         if (leftValue >= 0 && rightValue >= 0) {
-            return node.value;
+            return treeNode.val;
         } else {
             // find 0 or 1
             return leftValue + rightValue + 1;
         }
     }
 
-    public Node buildTree(int[] nums, int pos) {
+    public TreeNode buildTree(int[] nums, int pos) {
         if (nums == null || nums.length == 0 || pos >= nums.length || nums[pos] == -1) {
             return null;
         }
-        Node node = new Node(nums[pos]);
-        node.left = buildTree(nums, pos * 2 + 1);
-        node.right = buildTree(nums, pos * 2 + 2);
-        return node;
+        TreeNode treeNode = new TreeNode(nums[pos]);
+        treeNode.left = buildTree(nums, pos * 2 + 1);
+        treeNode.right = buildTree(nums, pos * 2 + 2);
+        return treeNode;
     }
 
     public static void main(String[] args) {
         int[] nums = {6, 2, 8, 0, 4, 7, 9, -1, -1, 3, 5};
         Ancestor s = new Ancestor();
-        Node node = s.buildTree(nums, 0);
-        System.out.println(s.findAncestor(node, 2, 4));
+        TreeNode treeNode = s.buildTree(nums, 0);
+        System.out.println(s.findAncestor(treeNode, 2, 4));
     }
 
 }
 
-class Node {
-    int value;
-    Node left;
-    Node right;
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
 
-    public Node(int value) {
-        this.value = value;
+    public TreeNode(int val) {
+        this.val = val;
     }
 }
