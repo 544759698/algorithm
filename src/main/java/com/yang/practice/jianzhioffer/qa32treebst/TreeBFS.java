df51117264510db127982cbe58aa2310ffad464c
yangguojun <544759698@qq.com>
2022-02-09 22:37:06 +0800

    day20220209

--- src/main/java/com/yang/practice/jianzhioffer/qa32treebst/TreeBFS.java
+++ src/main/java/com/yang/practice/jianzhioffer/qa32treebst/TreeBFS.java
@@ -10,7 +10,7 @@ import java.util.Queue;
  * @Date: 2022/2/9
  */
 public class TreeBFS {
-    public int[] levelOrder(TreeNode root) {
+    public int[] levelOrder1(TreeNode root) {
         List<Integer> ret = new ArrayList<>();
         if (root == null) {
             return new int[0];
@@ -35,6 +35,66 @@ public class TreeBFS {
             retArr[i] = ret.get(i);
         }
         return retArr;
+    }
+
+    public List<List<Integer>> levelOrder2(TreeNode root) {
+        List<List<Integer>> ret = new ArrayList<>();
+        if (root == null) {
+            return ret;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> item = new ArrayList<>();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode top = queue.poll();
+                item.add(top.val);
+                if (top.left != null) {
+                    queue.offer(top.left);
+                }
+                if (top.right != null) {
+                    queue.offer(top.right);
+                }
+            }
+            ret.add(item);
+        }
+        return ret;
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ret = new ArrayList<>();
+        if (root == null) {
+            return ret;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> item = new ArrayList<>();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode top = queue.poll();
+                item.add(top.val);
+                if (top.left != null) {
+                    queue.offer(top.left);
+                }
+                if (top.right != null) {
+                    queue.offer(top.right);
+                }
+            }
+            ret.add(item);
+        }
+        for (int i = 0; i < ret.size(); i++) {
+            if (i % 2 == 1) {
+                List<Integer> originalList = ret.get(i);
+                List<Integer> reverseList = new ArrayList<>();
+                for (int j = originalList.size() - 1; j >= 0; j--) {
+                    reverseList.add(originalList.get(j));
+                }
+                ret.set(i, reverseList);
+            }
+        }
+        return ret;
     }
 }
 
