2c00911d5e6e2bf70ba3c17509e9e0754cfac7c9
yangguojun <544759698@qq.com>
2022-03-03 01:14:04 +0800

    day20220303

--- src/main/java/com/yang/practice/hot100/qa739dailytemperature/DailyTemperature.java
+++ src/main/java/com/yang/practice/hot100/qa739dailytemperature/DailyTemperature.java
@@ -1,11 +1,14 @@
 package com.yang.practice.hot100.qa739dailytemperature;
 
+import java.util.Stack;
+
 /**
  * @Author: yangguojun01
  * @Date: 2022/3/2
  */
 public class DailyTemperature {
-    public int[] dailyTemperatures(int[] temperatures) {
+
+    public int[] dailyTemperatures1(int[] temperatures) {
         int[] ans = new int[temperatures.length];
         for (int i = 0; i < temperatures.length; i++) {
             int j = i + 1;
@@ -19,6 +22,19 @@ public class DailyTemperature {
             if (j >= temperatures.length) {
                 ans[i] = 0;
             }
+        }
+        return ans;
+    }
+
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> stack = new Stack<>();
+        int[] ans = new int[temperatures.length];
+        for (int i = 0; i < temperatures.length; i++) {
+            while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int idx = stack.pop();
+                ans[idx] = i - idx;
+            }
+            stack.push(i);
         }
         return ans;
     }
