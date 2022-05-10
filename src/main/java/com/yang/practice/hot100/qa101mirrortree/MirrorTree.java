9e385be24b4845392d30489608df4fcf83dd4acc
yangguojun <544759698@qq.com>
2022-05-10 20:27:23 +0800

    Update MirrorTree.java

--- src/main/java/com/yang/practice/hot100/qa101mirrortree/MirrorTree.java
+++ src/main/java/com/yang/practice/hot100/qa101mirrortree/MirrorTree.java
@@ -10,7 +10,22 @@ import java.util.Queue;
  * @Date: 2022/2/24
  */
 public class MirrorTree {
+
     public boolean isSymmetric(TreeNode root) {
+        return check(root, root);
+    }
+
+    public boolean check(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        return p.val == q.val && check(p.left, q.right) && check(p.right, q.left);
+    }
+
+    public boolean isSymmetric1(TreeNode root) {
         Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
         while (!queue.isEmpty()) {
