58bad8883135f44c19b9b3e0d802b8686156154b
yangguojun <544759698@qq.com>
2022-06-04 11:11:00 +0800

    Update MatrixPath.java

--- src/main/java/com/yang/practice/jianzhioffer/qa12matrixpath/MatrixPath.java
+++ src/main/java/com/yang/practice/jianzhioffer/qa12matrixpath/MatrixPath.java
@@ -5,49 +5,48 @@ package com.yang.practice.jianzhioffer.qa12matrixpath;
  * @Date: 2022/2/16
  */
 public class MatrixPath {
-    // TODO 没做对
+
     public boolean exist(char[][] board, String word) {
         int m = board.length;
         int n = board[0].length;
         boolean[][] visited = new boolean[m][n];
-        boolean ret = false;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                ret = checkPath(visited, i, j, "", board, word);
+                boolean ret = checkPath(board, i, j, visited, word, 0);
                 if (ret) {
-                    break;
+                    // 这里直接return
+                    return true;
                 }
             }
         }
-        return ret;
+        return false;
     }
 
-    public boolean checkPath(boolean[][] visited, int i, int j, String str, char[][] board, String word) {
-        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length) {
+    public boolean checkPath(char[][] board, int i, int j, boolean[][] visited, String word, int k) {
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || k > word.length() - 1
+                || board[i][j] != word.charAt(k) || visited[i][j]) {
             return false;
         }
-        if (visited[i][j]) {
-            return false;
-        }
-        String newStr = str + board[i][j];
-        if (newStr == word) {
+        if (board[i][j] == word.charAt(k) && k == word.length() - 1) {
             return true;
         }
         visited[i][j] = true;
-        if (checkPath(visited, i + 1, j, newStr, board, word)) {
-            return true;
-        }
-        if (checkPath(visited, i - 1, j, newStr, board, word)) {
-            return true;
-        }
-        if (checkPath(visited, i, j + 1, newStr, board, word)) {
-            return true;
-        }
-        if (checkPath(visited, i, j - 1, newStr, board, word)) {
+        if (checkPath(board, i + 1, j, visited, word, k + 1) || checkPath(board, i - 1, j, visited, word, k + 1)
+                || checkPath(board, i, j + 1, visited, word, k + 1) || checkPath(board, i, j - 1, visited, word,
+                k + 1)) {
             return true;
         }
         visited[i][j] = false;
         return false;
+    }
+
+    public static void main(String[] args) {
+        //        char[][] board = {{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}};
+        //        String word = "ABCCEDFFF";
+        char[][] board = {{'a'}, {'a'}};
+        String word = "aaa";
+        MatrixPath mp = new MatrixPath();
+        System.out.println(mp.exist(board, word));
     }
 
 }
