16c4318630c3c5ad1f50ce2e22099f1a244a32a5
yangguojun <544759698@qq.com>
2022-06-04 10:41:05 +0800

    Update MinInArray.java

--- src/main/java/com/yang/practice/jianzhioffer/qa11mininarray/MinInArray.java
+++ src/main/java/com/yang/practice/jianzhioffer/qa11mininarray/MinInArray.java
@@ -5,6 +5,23 @@ package com.yang.practice.jianzhioffer.qa11mininarray;
  * @Date: 2022/2/15
  */
 public class MinInArray {
+
+    public int minArray(int[] numbers) {
+        int low = 0;
+        int high = numbers.length - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (numbers[mid] > numbers[high]) {
+                low = mid + 1;
+            } else if (numbers[mid] < numbers[high]) {
+                high = mid;
+            } else {
+                high--;
+            }
+        }
+        return numbers[low];
+    }
+
     public int minArray1(int[] numbers) {
         if (numbers == null || numbers.length == 0) {
             return -1;
