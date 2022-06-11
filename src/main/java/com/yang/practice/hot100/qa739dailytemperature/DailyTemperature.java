42bb6fcdeb249309b749c11eac1c6b7526d2317e
yangguojun <544759698@qq.com>
2022-06-11 18:36:07 +0800

    Update DailyTemperature.java

--- src/main/java/com/yang/practice/hot100/qa739dailytemperature/DailyTemperature.java
+++ src/main/java/com/yang/practice/hot100/qa739dailytemperature/DailyTemperature.java
@@ -27,15 +27,18 @@ public class DailyTemperature {
     }
 
     public int[] dailyTemperatures(int[] temperatures) {
-        Stack<Integer> stack = new Stack<>();
         int[] ans = new int[temperatures.length];
+        // 栈里存索引
+        Stack<Integer> stack = new Stack<>();
         for (int i = 0; i < temperatures.length; i++) {
+            // 新数据大于栈顶索引的元素，则栈顶索引元素第一个大于它的元素可以找到，同时弹出栈顶元素
             while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
-                int idx = stack.pop();
-                ans[idx] = i - idx;
+                int preIdx = stack.pop();
+                ans[preIdx] = i - preIdx;
             }
             stack.push(i);
         }
         return ans;
     }
+
 }
