50b4ea16abac7959f9455b1ac57a62e677468d62
yangguojun <544759698@qq.com>
2022-06-12 17:39:45 +0800

    Update SerializeTree.java

--- src/main/java/com/yang/practice/jianzhioffer/qa37serializetree/SerializeTree.java
+++ src/main/java/com/yang/practice/jianzhioffer/qa37serializetree/SerializeTree.java
@@ -45,6 +45,20 @@ public class SerializeTree {
         return root;
     }
 
+    public static void main(String[] args) {
+        TreeNode n1 = new TreeNode(1);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(3);
+        TreeNode n4 = new TreeNode(4);
+        TreeNode n5 = new TreeNode(5);
+        n1.left = n2;
+        n1.right = n3;
+        n3.left = n4;
+        n3.right = n5;
+        SerializeTree st = new SerializeTree();
+        System.out.println(st.serialize(n1));
+    }
+
 }
 
 class TreeNode {
