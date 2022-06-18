ff836d25616be53a2923198e4427073c8bedb8b0
yangguojun <544759698@qq.com>
2022-06-18 23:46:15 +0800

    Create TranslateNum.java

---
+++ src/main/java/com/yang/practice/jianzhioffer/qa46translatenum/TranslateNum.java
@@ -0,0 +1,48 @@
+package com.yang.practice.jianzhioffer.qa46translatenum;
+
+/**
+ * @Author: yangguojun01
+ * @Date: 2022/6/18
+ */
+public class TranslateNum {
+
+    public int translateNum(int num) {
+        String strNum = String.valueOf(num);
+        int[] dp = new int[strNum.length()];
+        dp[0] = 1;
+        int tmp = 0;
+        if (strNum.length() > 1) {
+            tmp = (strNum.charAt(0) - '0') * 10 + (strNum.charAt(1) - '0');
+            if (tmp >= 10 && tmp <= 25) {
+                dp[1] = 2;
+            } else {
+                dp[1] = 1;
+            }
+        }
+
+        for (int i = 2; i < strNum.length(); i++) {
+            dp[i] = dp[i - 1];
+            tmp = (strNum.charAt(i - 1) - '0') * 10 + (strNum.charAt(i) - '0');
+            if (tmp >= 10 && tmp <= 25) {
+                dp[i] = dp[i] + dp[i - 2];
+            }
+        }
+        return dp[strNum.length() - 1];
+    }
+
+    public int translateNum1(int num) {
+        String strNum = String.valueOf(num);
+        int[] dp = new int[strNum.length() + 1];
+        dp[0] = 1; // eg:num=12,dp[2]=2
+        dp[1] = 1;
+        for (int i = 2; i < strNum.length() + 1; i++) {
+            int tmp = (strNum.charAt(i - 2) - '0') * 10 + (strNum.charAt(i - 1) - '0');
+            if (tmp >= 10 && tmp <= 25) {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            } else {
+                dp[i] = dp[i - 1];
+            }
+        }
+        return dp[strNum.length()];
+    }
+}
