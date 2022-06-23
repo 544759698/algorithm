939413049a2b5fd5d1a18c31d21e335c49a04a4c
yangguojun <544759698@qq.com>
2022-06-23 22:35:41 +0800

    Update SameNode.java

--- src/main/java/com/yang/practice/jianzhioffer/qa52samenode/SameNode.java
+++ src/main/java/com/yang/practice/jianzhioffer/qa52samenode/SameNode.java
@@ -7,7 +7,19 @@ import java.util.Stack;
  * @Date: 2022/2/19
  */
 public class SameNode {
+
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode pa = headA;
+        ListNode pb = headB;
+        while (pa != pb) {
+            // 这里是判断 pa==null 不是 pa.next==null，不然会时间超限
+            pa = pa == null ? headB : pa.next;
+            pb = pb == null ? headA : pb.next;
+        }
+        return pa;
+    }
+
+    public ListNode getIntersectionNode1(ListNode headA, ListNode headB) {
         ListNode ret = null;
         if (headA == null || headB == null) {
             return ret;
