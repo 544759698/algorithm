532af90451c4d9c5cf0752d5703c373e44e6d8e6
yangguojun <544759698@qq.com>
2022-06-26 20:42:50 +0800

    Update PrintMatrix.java

--- src/main/java/com/yang/practice/jianzhioffer/qa29printmatrix/PrintMatrix.java
+++ src/main/java/com/yang/practice/jianzhioffer/qa29printmatrix/PrintMatrix.java
@@ -2,12 +2,13 @@ package com.yang.practice.jianzhioffer.qa29printmatrix;
 
 /**
  * TODO 需要练习 1
+ *
  * @Author: yangguojun01
  * @Date: 2022/3/6
  */
 public class PrintMatrix {
     public int[] spiralOrder(int[][] matrix) {
-        if (matrix.length == 0 || matrix[0].length == 0) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
             return new int[0];
         }
         int m = matrix.length;
@@ -16,10 +17,9 @@ public class PrintMatrix {
         int[][] direction = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
         int directionIdx = 0;
         int row = 0, col = 0;
-        int retIdx = 0;
         boolean[][] visited = new boolean[m][n];
-        while (retIdx < ret.length) {
-            ret[retIdx] = matrix[row][col];
+        for (int i = 0; i < m * n; i++) {
+            ret[i] = matrix[row][col];
             visited[row][col] = true;
             int nextRow = row + direction[directionIdx][0];
             int nextCol = col + direction[directionIdx][1];
@@ -29,7 +29,6 @@ public class PrintMatrix {
             }
             row = row + direction[directionIdx][0];
             col = col + direction[directionIdx][1];
-            retIdx++;
         }
         return ret;
     }
