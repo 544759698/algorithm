3c7f3b01246d49d5c88134b19ea2f75c384ef2c8
yangguojun <544759698@qq.com>
2022-07-02 14:32:16 +0800

    Update TranslateNum.java

--- src/main/java/com/yang/practice/jianzhioffer/qa46translatenum/TranslateNum.java
+++ src/main/java/com/yang/practice/jianzhioffer/qa46translatenum/TranslateNum.java
@@ -1,7 +1,7 @@
 package com.yang.practice.jianzhioffer.qa46translatenum;
 
 /**
- * TODO 需要复习
+ * TODO 需要复习 1
  *
  * @Author: yangguojun01
  * @Date: 2022/6/18
@@ -9,43 +9,22 @@ package com.yang.practice.jianzhioffer.qa46translatenum;
 public class TranslateNum {
 
     public int translateNum(int num) {
-        String strNum = String.valueOf(num);
-        int[] dp = new int[strNum.length()];
+        String numStr = String.valueOf(num);
+        int[] dp = new int[numStr.length()];
         dp[0] = 1;
-        int tmp = 0;
-        if (strNum.length() > 1) {
-            tmp = (strNum.charAt(0) - '0') * 10 + (strNum.charAt(1) - '0');
-            if (tmp >= 10 && tmp <= 25) {
-                dp[1] = 2;
-            } else {
-                dp[1] = 1;
-            }
-        }
-
-        for (int i = 2; i < strNum.length(); i++) {
-            dp[i] = dp[i - 1];
-            tmp = (strNum.charAt(i - 1) - '0') * 10 + (strNum.charAt(i) - '0');
-            if (tmp >= 10 && tmp <= 25) {
-                dp[i] = dp[i] + dp[i - 2];
-            }
-        }
-        return dp[strNum.length() - 1];
-    }
-
-    public int translateNum1(int num) {
-        String strNum = String.valueOf(num);
-        int[] dp = new int[strNum.length() + 1];
-        // eg:num=12,dp[2]=2
-        dp[0] = 1;
-        dp[1] = 1;
-        for (int i = 2; i < strNum.length() + 1; i++) {
-            int tmp = (strNum.charAt(i - 2) - '0') * 10 + (strNum.charAt(i - 1) - '0');
-            if (tmp >= 10 && tmp <= 25) {
-                dp[i] = dp[i - 1] + dp[i - 2];
+        for (int i = 1; i < numStr.length(); i++) {
+            int tmp = (numStr.charAt(i - 1) - '0') * 10 + (numStr.charAt(i) - '0');
+            if (tmp > 9 && tmp < 26) {
+                if (i > 1) {
+                    dp[i] = dp[i - 1] + dp[i - 2];
+                } else {
+                    dp[1] = 2;
+                }
             } else {
                 dp[i] = dp[i - 1];
             }
         }
-        return dp[strNum.length()];
+        return dp[numStr.length() - 1];
     }
+
 }
