14dadbbf280c5bb0ae25568e0d7f7c1aaca4bbb1
yangguojun <544759698@qq.com>
2022-07-05 23:12:26 +0800

    Update BalanceTree.java

--- src/main/java/com/yang/practice/jianzhioffer/qa55balancetree/BalanceTree.java
+++ src/main/java/com/yang/practice/jianzhioffer/qa55balancetree/BalanceTree.java
@@ -1,18 +1,40 @@
 package com.yang.practice.jianzhioffer.qa55balancetree;
 
 /**
+ * TODO 自顶向下和自底向上
+ *
  * @Author: yangguojun01
  * @Date: 2022/2/10
  */
 public class BalanceTree {
+
+    // 自底向上 O(n)
     public boolean isBalanced(TreeNode root) {
+        return height(root) >= 0;
+    }
+
+    public int height(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = height(root.left);
+        int right = height(root.right);
+        if (left == -1 || right == -1 || Math.abs(left - right) > 1) {
+            return -1;
+        } else {
+            return Math.max(left, right) + 1;
+        }
+    }
+
+    // 自顶向下 O(n2) 下面的节点需要多次遍历
+    public boolean isBalanced1(TreeNode root) {
         if (root == null) {
             return true;
         }
         if (Math.abs(getHeight(root.left) - getHeight(root.right)) > 1) {
             return false;
         }
-        return isBalanced(root.left) && isBalanced(root.right);
+        return isBalanced1(root.left) && isBalanced1(root.right);
     }
 
     public int getHeight(TreeNode root) {
