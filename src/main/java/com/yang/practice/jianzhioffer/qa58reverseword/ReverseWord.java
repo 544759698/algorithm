1483d2b4da26fc84c90f65f88fee762ce9cf2b10
yangguojun <544759698@qq.com>
2022-07-09 18:31:39 +0800

    Update ReverseWord.java

--- src/main/java/com/yang/practice/jianzhioffer/qa58reverseword/ReverseWord.java
+++ src/main/java/com/yang/practice/jianzhioffer/qa58reverseword/ReverseWord.java
@@ -5,7 +5,28 @@ package com.yang.practice.jianzhioffer.qa58reverseword;
  * @Date: 2022/3/20
  */
 public class ReverseWord {
+
+    // 双指针 内存占用小 速度慢
     public String reverseWords(String s) {
+        if (s == null || s.length() == 0) {
+            return s;
+        }
+        //String[] words = s.trim().split(" "); //这种split会把多个空格的也拆出一个空格来
+        String[] words = s.trim().split("\\s+");
+        int left = 0;
+        int right = words.length - 1;
+        while (left < right) {
+            String word = words[left];
+            words[left] = words[right];
+            words[right] = word;
+            left++;
+            right--;
+        }
+        return String.join(" ", words);
+    }
+
+    // 速度快一点
+    public String reverseWords1(String s) {
         String[] words = s.trim().split(" ");
         StringBuilder sb = new StringBuilder();
         for (int i = words.length - 1; i >= 0; i--) {
@@ -15,4 +36,11 @@ public class ReverseWord {
         }
         return sb.toString().trim();
     }
+
+    public static void main(String[] args) {
+        String s = "a good   example";
+        ReverseWord r = new ReverseWord();
+        System.out.println(r.reverseWords(s));
+    }
+
 }
