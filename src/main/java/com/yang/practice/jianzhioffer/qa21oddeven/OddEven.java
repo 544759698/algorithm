7b4ac9b0c2823997f43d16ca26bb2235c79b68ae
yangguojun <544759698@qq.com>
2022-08-01 23:30:28 +0800

    Update OddEven.java

--- src/main/java/com/yang/practice/jianzhioffer/qa21oddeven/OddEven.java
+++ src/main/java/com/yang/practice/jianzhioffer/qa21oddeven/OddEven.java
@@ -15,19 +15,23 @@ public class OddEven {
             while (j >= 0 && !isOdd(nums[j])) {
                 j--;
             }
-            if (i < j && i < nums.length && j >= 0) {
-                int tmp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = tmp;
-                i++;
-                j--;
+            if (j > i) {
+                swap(nums, i, j);
             }
         }
         return nums;
     }
 
+    // 这里不封装函数leetcode耗时会变成1ms，封装了变成3ms
     private boolean isOdd(int num) {
-        return (num & 1) == 1;
+        //        return (num & 1) == 1;
+        return num % 2 == 1;
+    }
+
+    public void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
     }
 
 }
