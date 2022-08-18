10fea1ce26a0998ec5ec5dd4b0a3b6f99a796fea
yangguojun <544759698@qq.com>
2022-08-18 23:01:41 +0800

    Create SameTree.java

---
+++ src/main/java/com/yang/practice/hot100/qa100sametree/SameTree.java
@@ -0,0 +1,36 @@
+package com.yang.practice.hot100.qa100sametree;
+
+/**
+ * @Author: yangguojun01
+ * @Date: 2022/8/18
+ */
+public class SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null || p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
