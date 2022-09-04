3a8bd7ba86d4b1c1c418651def728939f5849844
yangguojun <544759698@qq.com>
2022-09-04 23:27:03 +0800

    Update NoRepeatSubString.java

--- src/main/java/com/yang/practice/hot100/qa3norepeatsubstring/NoRepeatSubString.java
+++ src/main/java/com/yang/practice/hot100/qa3norepeatsubstring/NoRepeatSubString.java
@@ -1,15 +1,32 @@
 package com.yang.practice.hot100.qa3norepeatsubstring;
 
+import java.util.HashMap;
 import java.util.HashSet;
 
 /**
- * TODO 不是最优解
- *
  * @Author: yangguojun01
  * @Date: 2022/2/18
  */
 public class NoRepeatSubString {
+
     public int lengthOfLongestSubstring(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        int left = -1;
+        HashMap<Character, Integer> map = new HashMap<>();
+        int ret = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                left = Math.max(left, map.get(s.charAt(i)));
+            }
+            map.put(s.charAt(i), i);
+            ret = Math.max(ret, i - left);
+        }
+        return ret;
+    }
+
+    public int lengthOfLongestSubstring1(String s) {
         int maxLength = 0;
         if (s == null || s.length() == 0) {
             return maxLength;
