a22bd796c4dd303e4e712b25ea84c12a55ad93f3
yangguojun <544759698@qq.com>
2022-09-14 23:37:27 +0800

    Create CircleList.java

---
+++ src/main/java/com/yang/practice/hot100/qa141circlelist/CircleList.java
@@ -0,0 +1,30 @@
+package com.yang.practice.hot100.qa141circlelist;
+
+/**
+ * @Author: yangguojun01
+ * @Date: 2022/9/14
+ */
+public class CircleList {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
