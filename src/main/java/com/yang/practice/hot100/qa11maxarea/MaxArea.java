96262e29000f441343a187a89ae6abca881195e9
yangguojun <544759698@qq.com>
2022-09-26 22:49:05 +0800

    Update MaxArea.java

--- src/main/java/com/yang/practice/hot100/qa11maxarea/MaxArea.java
+++ src/main/java/com/yang/practice/hot100/qa11maxarea/MaxArea.java
@@ -5,16 +5,6 @@ package com.yang.practice.hot100.qa11maxarea;
  * @Date: 2022/2/28
  */
 public class MaxArea {
-    public int maxArea1(int[] height) {
-        int maxArea = 0;
-        for (int i = 0; i < height.length - 1; i++) {
-            for (int j = i + 1; j < height.length; j++) {
-                int area = (j - i) * Math.min(height[i], height[j]);
-                maxArea = Math.max(maxArea, area);
-            }
-        }
-        return maxArea;
-    }
 
     public int maxArea(int[] height) {
         int maxArea = 0;
