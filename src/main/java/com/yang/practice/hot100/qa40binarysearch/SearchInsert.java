d01af5bab0be3c99aa8a3af1a0b10bf4aa4c9271
yangguojun <544759698@qq.com>
2022-10-02 00:09:17 +0800

    Create SearchInsert.java

---
+++ src/main/java/com/yang/practice/hot100/qa40binarysearch/SearchInsert.java
@@ -0,0 +1,59 @@
+package com.yang.practice.hot100.qa40binarysearch;
+
+/**
+ * 二分查找
+ *
+ * @Author: yangguojun01
+ * @Date: 2022/10/1
+ */
+public class SearchInsert {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return nums[left] >= target ? left : left + 1;
+    }
+
+    // 标准二分
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + ((right - left) >> 1);
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    // 本题标准二分实现
+    public int searchInsert1(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+}
