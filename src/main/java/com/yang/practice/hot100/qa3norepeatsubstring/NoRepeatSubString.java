37e640805474b83e54201edcb169c84d8ccd3b38
yangguojun <544759698@qq.com>
2022-10-31 20:28:13 +0800

    Update NoRepeatSubString.java

--- src/main/java/com/yang/practice/hot100/qa3norepeatsubstring/NoRepeatSubString.java
+++ src/main/java/com/yang/practice/hot100/qa3norepeatsubstring/NoRepeatSubString.java
@@ -34,6 +34,7 @@ public class NoRepeatSubString {
          */
         for (int i = 0; i < s.length(); i++) {
             if (map.containsKey(s.charAt(i))) {
+                // TODO REMEMBER
                 left = Math.max(left, map.get(s.charAt(i)));
             }
             map.put(s.charAt(i), i);
