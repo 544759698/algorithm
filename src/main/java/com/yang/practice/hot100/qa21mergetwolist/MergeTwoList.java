0650296ea0752b5ad59abeeececbd7470bb0196a
yangguojun <544759698@qq.com>
2022-11-03 00:17:15 +0800

    Update MergeTwoList.java

--- src/main/java/com/yang/practice/hot100/qa21mergetwolist/MergeTwoList.java
+++ src/main/java/com/yang/practice/hot100/qa21mergetwolist/MergeTwoList.java
@@ -23,21 +23,6 @@ public class MergeTwoList {
         return dump.next;
     }
 
-    public ListNode mergeTwoLists1(ListNode list1, ListNode list2) {
-        ListNode pre = new ListNode(-1);
-        ListNode dump = pre;
-        while (list1 != null || list2 != null) {
-            if ((list1 != null && list2 == null) || ((list1 != null && list2 != null) && list1.val < list2.val)) {
-                pre.next = list1;
-                list1 = list1.next;
-            } else {
-                pre.next = list2;
-                list2 = list2.next;
-            }
-            pre = pre.next;
-        }
-        return dump.next;
-    }
 }
 
 class ListNode {
