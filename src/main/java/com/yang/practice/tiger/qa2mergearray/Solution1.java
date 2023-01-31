37f5777c4b787ea514ebd9605337083abd825299
yangguojun <544759698@qq.com>
2023-01-31 23:52:47 +0800

    Update Solution1.java

--- src/main/java/com/yang/practice/tiger/qa2mergearray/Solution1.java
+++ src/main/java/com/yang/practice/tiger/qa2mergearray/Solution1.java
@@ -25,6 +25,24 @@ public class Solution1 {
         }
     }
 
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        m = m - 1;
+        n = n - 1;
+        while (m >= 0 && n >= 0) {
+            if (nums1[m] > nums2[n]) {
+                nums1[m + n + 1] = nums1[m];
+                m--;
+            } else {
+                nums1[m + n + 1] = nums2[n];
+                n--;
+            }
+        }
+        while (n >= 0) {
+            nums1[n] = nums2[n];
+            n--;
+        }
+    }
+
     public static void main(String[] args) {
         Solution1 s1 = new Solution1();
         int[] num1 = new int[] {2, 3, 6, 0, 0, 0};
