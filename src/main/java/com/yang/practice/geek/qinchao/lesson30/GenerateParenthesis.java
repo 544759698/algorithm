2cf0e54d73f61edbdb3ac1dafd96e058658ab141
yangguojun <544759698@qq.com>
2023-02-16 19:06:06 +0800

    Update GenerateParenthesis.java

--- src/main/java/com/yang/practice/geek/qinchao/lesson30/GenerateParenthesis.java
+++ src/main/java/com/yang/practice/geek/qinchao/lesson30/GenerateParenthesis.java
@@ -6,6 +6,7 @@ import java.util.List;
 /**
  * TODO 待复习
  * leetcode22 数字n代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且有效的括号组合。
+ * https://leetcode.cn/problems/generate-parentheses/
  *
  * @Author: yangguojun01
  * @Date: 2022/1/6
